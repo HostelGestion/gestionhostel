573b99ce3606dd4de37d2c25dc4a1894950abdca
matiasmacaya <matiasmacaya@gmail.com>
2016-07-02 00:51:43 -0300

    issue

---
+++ dom/src/main/java/domainapp/dom/simple/ListaPais.java
@@ -0,0 +1,366 @@
+package domainapp.dom.simple;
+
+public enum ListaPais {
+	Afghanistán,
+
+	Albania,
+
+	Alemania,
+
+	Andorra,
+
+	Angola,
+
+	Anguilla,
+
+	Antigua_y_Barbuda,
+
+	Arabia_Saudita,
+
+	Argelia,
+
+	Argentina,
+
+	Armenia,
+
+	Australia,
+
+	Austria,
+
+	Azerbayán,       
+
+	Bahamas,
+
+	Bahrein,
+
+	Bangladesh,
+
+	Barbados,
+
+	Belarús,
+
+	Bélgica,
+
+	Belice,
+
+	Benin,
+
+	Bhután,
+
+	Birmania,
+
+	Bolivia,
+
+	BosniaHerzegovina,
+
+	Botswana,
+
+	Brasil,
+
+	Brunei,
+
+	Bulgaria,
+
+	Burkina_Faso,
+
+	Burundi,             
+
+	Cabo_Verde,
+
+	Camboya,
+
+	Camerún,
+
+	Canadá,
+
+	Chile,
+
+	China,
+
+	Chipre,
+
+	Colombia,
+
+	Comoras,
+
+	Corea_del_Sur,
+
+	CostadeMarfil,
+
+	CostaRica,
+
+	Croacia,
+
+	Cuba,
+
+	Dinamarca,
+
+	Djibouti,             
+
+	Ecuador,
+
+	Egipto,
+
+	ElSalvador,
+
+	EmiratosArabesUnidos,
+
+	Eritrea,
+
+	Eslovaquia,
+
+	Eslovenia,
+
+	España,
+
+	EstadosUnidos,
+
+	Estonia,
+
+	Etiopia,
+
+	Fiji,
+
+	Filipinas,
+
+	Finlandia,
+
+	Francia,
+
+	 Gabon,
+
+	Gambia,
+
+	Georgia,
+
+	Ghana,
+
+	Grecia,
+
+	Granada,
+
+	Guatemala,
+
+	Guinea,
+
+	Guinea_Bissau,
+
+	Guinea_Ecuatorial,
+
+	Guyana,               
+
+	Haiti,
+
+	Honduras,
+
+	Hungria,
+
+	Islandia,
+
+	India,
+
+	Indonesia,
+
+	Iran,
+
+	Iraq,
+
+	Irlanda,
+
+	Israel,
+
+	Italia,
+
+	Jamaica,
+
+	Japón,
+
+	Jordania,
+
+	Kazajstán,
+	Kenia,
+	Kirguistán,
+
+	Kiribati,
+
+	Kuwait,
+
+	Laos,
+
+	Letonia,
+
+	Libano,
+
+	Lesotho,
+
+	Liberia,
+
+	Libia,
+
+	Liechtenstein,
+
+	Lituania,
+
+	Luxemburgo,
+
+	Macedonia,
+
+	Madagascar,
+
+	Malawi,
+
+	Malasia,
+
+	Maldivas,
+
+	Mali,
+
+	Malta,
+
+	Marruecos,
+
+	Marshall,
+
+	Mauricio,
+
+	Mauritania,
+
+	México,
+
+	Naurú,
+
+	Nepal,
+
+	Nicaragua,
+
+	Niger,
+
+	Nigeria,
+
+	Noruega,
+
+	NuevaZelandia,
+
+	Omán,
+
+	PaísesBajos,
+
+	Pakistán,
+
+	Palau,
+
+	Panamá,
+
+	PapúaNuevaGuinea,
+
+	Paraguay,
+
+	Perú,
+
+	Polonia,
+
+	Portugal,
+	Qatar,
+	ReinoU,
+
+	RepCentroafricana,
+
+	RepCheca,
+
+	RepDominicana,
+
+	Ruanda,
+
+	Rumania,
+
+	Rusia,
+
+	SalomonIslands,
+
+	Samoa,
+
+	SanMarino,
+
+	SanCristóbalNevis,
+
+	SantaLucía,
+
+	Vaticano,
+
+	SãoToméyPrincipe,
+
+	StVincentelasGrenadinas,
+
+	Senegal,
+
+	Seychelles,
+
+	SierraLeona,
+
+	Singapur,
+
+	Siria,
+
+	Somalia,
+
+	SriLanka,
+
+	Sudáfrica,
+
+	Sudán,
+
+	Suecia,
+
+	Suiza,
+
+	Suriname,
+
+	Swazilandia,      
+
+	Tailandia,
+
+	Taiwán,
+
+	Tanzania,
+
+	Tayikistán,
+
+	Togo,
+
+	Tonga,
+
+	TrinidadyTabago,
+
+	Túnez,
+
+	Turkmenistan,
+
+	Turquia,
+
+	Tuvalu,
+
+	 
+
+	Ucraina,
+
+	Uganda,
+
+	Uruguay,
+
+	Uzbekistán,
+
+	Vanuatu,
+
+	Venezuela,
+
+	Vietnam,            
+
+	Yémen,
+
+	Yugoslavia,
+
+	Zambia,
+
+	Zimbabwe
+
+}
