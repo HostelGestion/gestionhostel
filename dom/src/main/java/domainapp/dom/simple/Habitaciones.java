4f7fe6691bff1ebeaa2486aee7644de72cffc0cd
clipablo <clipablo@gmail.com>
2016-08-16 21:37:42 -0300

    issue #4: Creacion de Habitacion
    Creacion de Habitacion unicamente el nombre

---
+++ dom/src/main/java/domainapp/dom/simple/Habitaciones.java
@@ -0,0 +1,101 @@
+
+package domainapp.dom.simple;
+
+import java.util.List;
+
+import org.apache.isis.applib.Identifier;
+import org.apache.isis.applib.annotation.Action;
+import org.apache.isis.applib.annotation.ActionLayout;
+import org.apache.isis.applib.annotation.BookmarkPolicy;
+import org.apache.isis.applib.annotation.DomainService;
+import org.apache.isis.applib.annotation.DomainServiceLayout;
+import org.apache.isis.applib.annotation.MemberOrder;
+import org.apache.isis.applib.annotation.NatureOfService;
+import org.apache.isis.applib.annotation.Optionality;
+import org.apache.isis.applib.annotation.Parameter;
+import org.apache.isis.applib.annotation.ParameterLayout;
+import org.apache.isis.applib.annotation.SemanticsOf;
+import org.apache.isis.applib.query.QueryDefault;
+import org.apache.isis.applib.services.eventbus.ActionDomainEvent;
+import org.apache.isis.applib.services.i18n.TranslatableString;
+import org.apache.isis.applib.services.repository.RepositoryService;
+
+
+@DomainService(
+        nature = NatureOfService.VIEW,
+        repositoryFor = Habitaciones.class
+)
+@DomainServiceLayout(
+        menuOrder = "10"
+)
+public class Habitaciones {
+
+    //region > title
+    public TranslatableString title() {
+        return TranslatableString.tr("Habitacion");
+    }
+    //endregion
+
+    //region > listAll (action)
+    @Action(
+            semantics = SemanticsOf.SAFE
+    )
+    @ActionLayout(
+            bookmarking = BookmarkPolicy.AS_ROOT
+    )
+    @MemberOrder(sequence = "1")
+    public List<Habitaciones> listAll() {
+        return repositoryService.allInstances(Habitaciones.class);
+    }
+    //endregion
+
+    //region > findByName (action)
+    @Action(
+            semantics = SemanticsOf.SAFE
+    )
+    @ActionLayout(
+            bookmarking = BookmarkPolicy.AS_ROOT
+    )
+    @MemberOrder(sequence = "2")
+    public List<Habitacion> findByName(
+            @ParameterLayout(named="Name")
+            final String name
+    ) {
+        return repositoryService.allMatches(
+                new QueryDefault<>(
+                		Habitacion.class,
+                        "findByName",
+                        "name", name));
+    }
+    //endregion
+
+    //region > create (action)
+    public static class CreateDomainEvent extends ActionDomainEvent<Habitaciones> {
+        public CreateDomainEvent(final Habitaciones source, final Identifier identifier, final Object... arguments) {
+            super(source, identifier, arguments);
+        }
+    }
+
+    @Action(
+            domainEvent = CreateDomainEvent.class
+    )
+    @MemberOrder(sequence = "1")
+    public TipodeHabitacion create(
+            final @ParameterLayout(named="Nombre") String name
+           ) {
+        final TipodeHabitacion obj = repositoryService.instantiate(TipodeHabitacion.class);
+        obj.setName(name);
+        
+        repositoryService.persist(obj);
+        return obj;
+    }
+
+    //endregion
+
+    //region > injected services
+
+    @javax.inject.Inject
+    RepositoryService repositoryService;
+
+    //endregion
+}
