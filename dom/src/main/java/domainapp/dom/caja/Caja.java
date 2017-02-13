747c4200ace13260d34238802f23f33d5bf50463
clipablo <clipablo@gmail.com>
2017-02-13 10:53:39 -0300

    issue #17: Reportes PDF con JasperReports
    Archivos modificados de clase caja para la cosntruccion de caja

--- dom/src/main/java/domainapp/dom/caja/Caja.java
+++ dom/src/main/java/domainapp/dom/caja/Caja.java
@@ -18,21 +18,36 @@
  */
 package domainapp.dom.caja;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.activation.MimeType;
+import javax.activation.MimeTypeParseException;
+import javax.jdo.annotations.Column;
 import javax.jdo.annotations.IdentityType;
 import javax.jdo.annotations.VersionStrategy;
 
 import org.apache.isis.applib.annotation.Action;
+import org.apache.isis.applib.annotation.ActionLayout;
 import org.apache.isis.applib.annotation.DomainObject;
 import org.apache.isis.applib.annotation.Property;
+import org.apache.isis.applib.annotation.PropertyLayout;
 import org.apache.isis.applib.annotation.SemanticsOf;
 import org.apache.isis.applib.services.eventbus.ActionDomainEvent;
 import org.apache.isis.applib.services.eventbus.PropertyDomainEvent;
 import org.apache.isis.applib.services.i18n.TranslatableString;
 import org.apache.isis.applib.services.repository.RepositoryService;
 import org.apache.isis.applib.util.ObjectContracts;
+import org.apache.isis.applib.value.Blob;
+import org.apache.isis.applib.annotation.MemberOrder;
 
 import domainapp.dom.huesped.Huesped;
-import domainapp.dom.reserva.Reserva;
+import domainapp.dom.reportes.GenerarReporte;
+import domainapp.dom.reportes.TicketReporte;
+import domainapp.dom.caja.RepoCaja;
+import net.sf.jasperreports.engine.JRException;
+import org.joda.time.LocalDate;
 
 @javax.jdo.annotations.PersistenceCapable(
         identityType=IdentityType.DATASTORE,
@@ -108,6 +123,7 @@ public class Caja implements Comparable<Caja> {
         this.monto = monto;
     }
     
+   /* private
     private String concepto;
     @javax.jdo.annotations.Column(allowsNull="false")
     public String getConcepto() {
@@ -115,20 +131,66 @@ public class Caja implements Comparable<Caja> {
     }
     public void setConcepto(final String concepto) {
         this.concepto = concepto;
-    }
-
+    }*/
+    
+    @MemberOrder(sequence = "2")
+	@Column(allowsNull="false")
+	@Property(domainEvent = NameDomainEvent.class)
+	@PropertyLayout(named="Fecha de Pago")
+	private LocalDate fechaDePago;
+	public LocalDate getFechaDePago() {return fechaDePago;}
+	public void setFechaDePago(LocalDate fechaDePago) {this.fechaDePago = fechaDePago;}
 
     public static class DeleteDomainEvent extends ActionDomainEvent<Caja> {}
     @Action(
             domainEvent = DeleteDomainEvent.class,
             semantics = SemanticsOf.NON_IDEMPOTENT_ARE_YOU_SURE
     )
+    @ActionLayout(named="Eliminar Ingreso")
     public void delete() {
         repositoryService.remove(this);
     }
 
-
-
+public String imprimirTicket() throws JRException{
+		
+		List<Object> objectsReport = new ArrayList<Object>();
+		
+		TicketReporte caja = new TicketReporte();
+			
+	//	Caja.setMonto(getMonto());
+		//caja.setValor(valor);// .setValor(); // .setMonto(getMonto());
+		//caja.setMonto(getMonto());
+		
+		//caja.setValor(null);
+		//caja.setMonto(getMonto());
+		//caja.setMonto(getMonto());
+		//caja.setHuesped(getHuesped());
+		//caja.setHuesped(huesped);
+	
+		caja.setFechaDePago(String.valueOf(getFechaDePago()));
+	
+		objectsReport.add(caja);
+		
+		String nombreArchivo ="reportes/Caja_"/* + String.valueOf(caja.getNroRecibo())*/ ;
+		
+		GenerarReporte.generarReporte("reportes/Ticketdepago.jrxml", objectsReport, nombreArchivo);
+		
+		return "Reporte Generado.";
+}
+    
+    @Action
+    public Blob descargar() {
+    	String palabra = "hola que tal";
+    	byte[] bitsDeArchivo=palabra.getBytes();
+    	MimeType tipoDeArchivo = null;
+    	try {
+			tipoDeArchivo=new MimeType("text/plain");
+		} catch (MimeTypeParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	return new Blob("archivo.txt",tipoDeArchivo,bitsDeArchivo);
+    }
     @Override
     public int compareTo(final Caja other) {
         return ObjectContracts.compare(this, other, "name");
@@ -137,5 +199,8 @@ public class Caja implements Comparable<Caja> {
 
     @javax.inject.Inject
     RepositoryService repositoryService;
+    
+    @javax.inject.Inject	
+	RepoCaja repoCaja;
 
 }
