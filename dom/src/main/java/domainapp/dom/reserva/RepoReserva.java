9b4973069fa03fa1891620359974bff063468f6c
clipablo <clipablo@gmail.com>
2017-03-28 22:16:46 -0300

    issue #5: Creacion de Reserva
    Error en modificacion

--- dom/src/main/java/domainapp/dom/reserva/RepoReserva.java
+++ dom/src/main/java/domainapp/dom/reserva/RepoReserva.java
@@ -167,7 +167,7 @@ public class RepoReserva {
             @ParameterLayout(named="Fecha salida") LocalDate fechaSal,
     		@ParameterLayout(named="Habitación") Habitacion habitacion,
     		@ParameterLayout(named="Huéspedes?") int numHues,
-
+    		@ParameterLayout(named="Canal de venta")@Parameter(optionality = Optionality.MANDATORY) String canalVenta
 
     		//@ParameterLayout(named="Estado")@Parameter(optionality = Optionality.OPTIONAL) String estado
     			
